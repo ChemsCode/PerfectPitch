5038ac7165a73bac8609dc618c7f82e2c4d8af66
ChemsCode <saidiceb@gmail.com>
2024-03-23 14:57:49 -0400

    modified hero section

--- frontend/src/components/Landing/HeroSection.jsx
+++ frontend/src/components/Landing/HeroSection.jsx
@@ -1,4 +1,4 @@
-import Typed from "react-typed";
+import { ReactTyped } from "react-typed";
 import { Link } from "react-router-dom";
 
 
@@ -6,7 +6,7 @@ const HeroSection = () => {
     return (
         <div className="text-white">
             <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
-                <p className="text-[#00df9a] font-bold p-2">
+                <p className="text-indigo-500 font-bold p-2">
                     IMPROVE YOUR INTERVIEW SKILLS
                 </p>
                 <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">
@@ -16,7 +16,7 @@ const HeroSection = () => {
                     <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4">
                         Get a job in
                     </p>
-                    <Typed
+                    <ReactTyped 
                         className="md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2"
                         strings={["TECH", "FINANCE", "CONSULTING"]}
                         typeSpeed={120}
@@ -28,7 +28,7 @@ const HeroSection = () => {
                     Achieve your dream job with our NLP-based interviewing tool for Behavioural Interviews
                 </p>
                 <Link to="/home">
-                <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black">
+                <button className="bg-indigo-500 w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black">
                     Get Started
                 </button>
                 </Link>
