9b05cd5322639061b9a28320d3e9b0c564ac8da5
ChemsCode <saidiceb@gmail.com>
2024-03-23 17:36:53 -0400

    added corsoptions, errorhandle and credentials to the index.js file

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,19 @@ require('dotenv').config();
 const PORT = process.env.PORT || 3500;
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const corsOptions = require("./config/corsOptions");
+const errorHandler = require('./middleware/errorHandler');
+const credentials = require('./middleware/credentials');
 
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(credentials);
 
+//serve static files
+app.use(express.static('public'));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
